85c61aaa3cc5a58341fa2374612e8861d5a533af
rajneesh-nielsen <rajneesh.mishra@nielsen.com>
2017-08-23 15:35:11 +0530

    changed

--- src/app/login.component.ts
+++ src/app/login.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { LoginService } from './login.service';
+import { Router } from '@angular/router';
 
 @Component({
 	'selector': 'login',
@@ -8,16 +9,27 @@ import { LoginService } from './login.service';
 
 export class LoginComponent {
 	login = {"username": "", "password": ""};
-	private response: string[];
+	private response;
+	private errorMsg;
 
-	constructor(private loginService: LoginService){}
+	constructor(private loginService: LoginService, private router: Router){}
 	
 	onSubmit() {
 		this.loginService.passLoginDetails(this.login).subscribe(val => {
-      this.response = val;
+
+			this.response = val;
+			
+      if(this.response.success == 1) {
+      	//assign the returned value to session variable in local storage
+      	localStorage.setItem('session', JSON.stringify(this.response));
+      	this.router.navigate(['/home']);
+      } else {
+      	this.errorMsg = 'Username or password did not match';
+      }
     },
     err => {
       console.log('Something went wrong!');
-    });
+    }
+    );
 	}
 }
